852b5e605147023a622f54a8fadecc28080626ec
Jane Jeon <me@janejeon.dev>
2021-07-24 00:17:27 -0400

    normalize before running thru clearURL

--- utils/normalize-url.js
+++ utils/normalize-url.js
@@ -3,7 +3,8 @@ import stripTrackers from './strip-trackers'
 
 export default function gen(normalizeUrlOptions) {
   return function normalize(url, stripTrackersOption) {
+    url = normalizeUrl(url, normalizeUrlOptions)
     if (stripTrackersOption) url = stripTrackers(url)
-    return normalizeUrl(url, normalizeUrlOptions)
+    return url
   }
 }
