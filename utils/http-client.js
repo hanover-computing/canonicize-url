7c7abd0a5567a380202d967405157cba36ea2087
Jane Jeon <me@janejeon.dev>
2021-08-24 21:39:50 -0400

    integrate got-ssrf

--- utils/http-client.js
+++ utils/http-client.js
@@ -1,15 +1,6 @@
-import got from 'got-scraping'
-import ssrfHookGen from './ssrf-protect'
+import { gotScraping } from 'got-scraping'
+import { gotSsrf } from 'got-ssrf'
 
 export default function httpClient(globalOpts) {
-  let instance = got.gotScraping.extend(globalOpts.gotOptions)
-  if (!globalOpts.gotOptions.proxyUrl) {
-    // This is only for people who don't want to set up proxy
-    instance = instance.extend({
-      hooks: {
-        beforeRequest: [ssrfHookGen(globalOpts)]
-      }
-    })
-  }
-  return instance
+  return gotScraping.extend(globalOpts.gotOptions, gotSsrf)
 }
