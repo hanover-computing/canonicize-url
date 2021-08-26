9344b40f7e41ebc5a035a6030d08cae7d9ef6ca6
Jane Jeon <me@janejeon.dev>
2021-08-26 01:32:29 -0400

    default to non-www and https

--- utils/normalize-url.js
+++ utils/normalize-url.js
@@ -1,10 +1,49 @@
 import normalizeUrl from 'normalize-url'
 import stripTrackers from './strip-trackers'
+import { URL } from 'url'
 
-export default function gen(normalizeUrlOptions) {
-  return function normalize(url, stripTrackersOption) {
-    url = normalizeUrl(url, normalizeUrlOptions)
-    if (stripTrackersOption) url = stripTrackers(url)
-    return url
+export default function gen(normalizeUrlOptions, dnsLookup, httpClient) {
+  return async function normalize(url) {
+    // We default to non-www, https links
+    const preferredOptions = {
+      stripWWW: true,
+      forceHttps: true
+    }
+
+    // Pass 1: try to force as much normalization as possible, knowing that this may break some links
+    url = normalizeUrl(url, {
+      ...normalizeUrlOptions,
+      ...preferredOptions
+    })
+
+    // Check 1: if the www-stripped domain exists...
+    if (url.includes('www')) {
+      const { hostname } = new URL(url)
+      try {
+        await dnsLookup(hostname)
+      } catch (err) {
+        // Pass 2: we can't resolve the www-stripped host at the DNS level, so we enable it
+        preferredOptions.stripWWW = false
+        url = normalizeUrl(url, {
+          ...normalizeUrlOptions,
+          ...preferredOptions
+        })
+      }
+    }
+
+    // Check 2: if the site doesn't support HTTPS...
+    try {
+      await httpClient.head(url)
+    } catch (err) {
+      // Pass 3: we can't reach the URL via HTTP HEAD request, so try downgrading to http
+      preferredOptions.forceHttps = false
+      url = normalizeUrl(url, {
+        ...normalizeUrlOptions,
+        ...preferredOptions
+      })
+    }
+
+    // always strip trackers for consistency (even if it means worse performance)!
+    return stripTrackers(url)
   }
 }
