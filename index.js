4a465868834516dab42c915164374da3ae4a562e
Jane Jeon <me@janejeon.dev>
2021-09-11 19:26:36 -0400

    add global timeout

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import QuickLRU from 'quick-lru'
+import pTimeout from 'p-timeout'
 import normalizeUrl from './utils/normalize-url.js'
 import httpClientGen from './utils/http-client.js'
 import dnsLookupGen from './utils/dns-lookup.js'
@@ -19,14 +20,15 @@ export default (
       request: 14000 // global timeout
     },
     cache: new QuickLRU({ maxSize: 1000 })
-  }
+  },
+  timeoutMs = 15000 // global timeout for the ENTIRE function, because I'm afraid of blocking the event loop w/ some of the more compute-intensive shit
 ) => {
   const httpClient = httpClientGen(gotOptions)
   const dnsLookup = dnsLookupGen(gotOptions.dnsCache)
   const normalize = normalizeUrl(normalizeUrlOptions, dnsLookup, httpClient)
 
   // Normalize URL so that we can search by URL.
-  return async function normalizePlus(url = '') {
+  async function normalizePlus(url = '') {
     debug('Normalizing URL %s', url)
 
     // 1. "Base" normalization using normalize-url + stripping trackers
@@ -41,4 +43,6 @@ export default (
     // At this point, the link will be completely normalized based on canonical links (if one exists)
     return res.url
   }
+
+  return url => pTimeout(normalizePlus(url), timeoutMs)
 }
