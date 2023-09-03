1c96227536787c7d89d5bae45b918798f6d946a7
Jane Jeon <git@janejeon.com>
2023-09-03 22:47:56 +0100

    throw on HTTPS cert errors

--- src/hooks/settings.ts
+++ src/hooks/settings.ts
@@ -1,10 +1,14 @@
 import type { InitHook } from 'got'
 
-// Sets default settings, such as the number of redirects, and the number of retries.
-// We need to follow the redirect to get the true "end" URL,
-// and we need to allow at least one retry since we want to prefer HTTPS version
-// and make the request with it, but if it fails, we need to retry it with the non-HTTPS version.
+// Sets default settings.
 export const overrideSettings: InitHook = (plainRequestOptions, options) => {
+  // Throw on HTTPS cert errors
+  options.https.rejectUnauthorized = true
+
+  // We need to follow the redirect to get the true "end" URL
   options.followRedirect = true
+
+  // We need to allow at least one retry since we want to prefer HTTPS version and make the request with it,
+  // but if it fails, we need to retry it with the non-HTTPS version.
   if (!options.retry.limit) options.retry.limit = 1
 }
